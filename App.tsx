2de57be669a448ea4f78618ad6d9aa0a87361b24
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-06-16 23:13:40 -0300

    feat(fonts): installed and configured fonts with theme file

--- App.tsx
+++ App.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_700Bold,
+  Poppins_500Medium,
+} from '@expo-google-fonts/poppins';
+
 import { Dashboard } from './src/screens/Dashboard';
 import theme from './src/screens/global/styles/theme';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_700Bold,
+    Poppins_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
