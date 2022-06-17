8f78f9713fdf5049064ff9425566c6bef018d266
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-06-16 23:05:08 -0300

    fix(styled-components): adding types to styled-components react native

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { Dashboard } from './src/screens/Dashboard';
+import theme from './src/screens/global/styles/theme';
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Dashboard />
-    </>
+    </ThemeProvider>
   );
 }
---
+++ src/@types/styled-components.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+import theme from '../screens/global/styles/theme';
+
+declare module 'styled-components' {
+  type Theme = typeof theme;
+  export interface DefaultTheme extends Theme {}
+}
